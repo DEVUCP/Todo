bfa38d165130a6eecc8545f18a4170de4556fdc6
DEVUCP <OMHN05@protonmail.com>
2024-08-19 00:58:32 +0300

    Added specification to 'HELP_MGS' for command format and specifying command with 'Help()'.

--- main.py
+++ main.py
@@ -32,12 +32,12 @@ MASTER_SEPERATOR = "|"
 
 # Help message dictionary
 HELP_MSG = {
-    '>help': 'Displays available commands',
-    '>new': 'Creates a new task with description and duration',
-    '>edit': 'Edits an existing task by index',
-    '>complete': 'Marks a task as complete by index',
-    '>delete': 'Deletes a task by index',
-    '>show': 'Displays details of a specific task by index',
+    '>help': 'Displays available commands\n ``>help <command>`` (to see more details about a command)',
+    '>new': 'Creates a new task with description and duration\n ``>new <description> | <duration>`` (duration format: ``10M 1H 1D``)',
+    '>edit': 'Edits an existing task by index\n ``>edit <index> | <new_description>``',
+    '>complete': 'Marks a task as complete by index\n ``>complete <index>``',
+    '>delete': 'Deletes a task by index\n ``>delete <index>``',
+    '>show': 'Displays details of a specific task by index\n ``>show <index>``',
     '>showall': 'Displays all tasks'
 }
 ACCEPTED_TIMEFORMAT = "%I:%M"
@@ -48,11 +48,14 @@ loaded = False
 async def on_ready():
     print(f'Logged in as {client.user.name} ({client.user.id})')
     global loaded
+    
     if os.path.exists("user_accounts.json") and not loaded:
        # Load accounts from file if it exists
        singletons.account_list = LoadAccounts()
        loaded = True
+
        print("All tasks loaded successfully!")
+
     elif not os.path.exists("user_accounts.json"):
         loaded = True
 
@@ -82,6 +85,7 @@ async def ExecuteAction(command : list[str], message : discord.Message):
     # Execute the appropriate action based on the command
     action = command[0].lower()
     command.pop(0)
+
     match action:
         case "help":
             await Help(command=command,message=message)
@@ -121,11 +125,25 @@ async def ExecuteAction(command : list[str], message : discord.Message):
 
 async def Help(command: list[str], message: discord.Message):
     """Displays available commands."""
+    # Create an embed with all available commands
     if not len(command):
-        # Create an embed with all available commands
         embed = discord.Embed(title="Available Commands", color=0x6abe30)
+    
         for cmd, description in HELP_MSG.items():
             embed.add_field(name=cmd, value=description, inline=False)
+        await message.reply(embed=embed)
+    
+    # Show detailed help for a specific command
+    elif len(command) == 1:
+    
+        if command[0] not in HELP_MSG and MASTER_PREFIX+command[0] not in HELP_MSG: # checks regular command and command with MASTER_PREFIX
+            return
+    
+        if MASTER_PREFIX not in command[0]: # adds MASTER_PREFIX to command if not present so that it can be found in HELP_MSG
+            command[0] = MASTER_PREFIX+command[0]
+        
+        embed = discord.Embed(title=f"Command: {command[0]}", color=0x6abe30)
+        embed.add_field(name="Description", value=HELP_MSG[command[0]], inline=False)
         await message.reply(embed=embed)
     else:
         await utils.ExceptionReplyEmbed(message=message, error="Error", desc="Invalid Arguments, `>help` doesn't take any arguments.")
@@ -213,31 +231,38 @@ async def ShowTask(task_id: int, message : discord.Message):
 async def ShowAllTasks(message: discord.Message):
     """Shows all tasks."""
     useracc = utils.GetAcc(message.author.id)
+    
     if not useracc:
         await utils.ExceptionReplyEmbed(message=message, error="Error: You have no tasks to show", desc="Use `>new` to create a new task.")
         return
+    
     if useracc.tasks:
         # Create and send embed for all tasks
         embed = discord.Embed(color=0x6abe30)
         embed = utils.GetTaskListEmbed(tasks=useracc.tasks,embed=embed)
         await message.reply(embed=embed)
+    
     else:
         await utils.ExceptionReplyEmbed(message=message, error="Error: You have no tasks to show", desc="Use `>new` to create a new task.")
 
 async def CompleteTask(task_id : str, message : discord.Message):
     """Completes a task."""
     useracc = utils.GetAcc(message.author.id)
+    
     if useracc:
         try:
             # Mark task as complete and remove due date
             useracc.tasks[int(task_id)].done = True
             useracc.tasks[int(task_id)].due = None
+
             embed = discord.Embed(title="Task Completed", description=f"Task {task_id+1} has been marked as complete.", color=0x6abe30)
             await message.reply(embed=embed)
             await message.add_reaction('✅')
+        
         except Exception as err:
             await utils.ExceptionReplyEmbed(message=message, error=f"Error: {err}", desc="Please check your task ID and try again.")
             return
+   
     else:
         await utils.ExceptionReplyEmbed(message=message, error="Error: Task not found", desc="Use `>showall` to see all your tasks and their IDs")
 
@@ -246,12 +271,15 @@ async def EditTask(command : list[str], message : discord.Message):
     useracc = utils.GetAcc(message.author.id)
     command = " ".join(command)
     command = command.split(MASTER_SEPERATOR)
+
     try:
         new_desc = command[1]
     except IndexError:
         await utils.ExceptionReplyEmbed(message=message, error="Error: Invalid command format", desc="Use `>edit <task_id> | <new_description>` to edit a task")
         return
+    
     task_indx = int(command[0]) -1 # Minus one to offset starting from 0 in any normal list. 
+    
     if useracc:
         try:
             if useracc.tasks[task_indx].done:
@@ -260,11 +288,14 @@ async def EditTask(command : list[str], message : discord.Message):
             if useracc.tasks[task_indx].IsDue():
                 await utils.ExceptionReplyEmbed(message=message, error="Error: Task is over-due")
                 return
+    
             # Update task description
             useracc.tasks[task_indx].description = new_desc
+    
             embed = discord.Embed(title="Task Edited", description=f"Task {task_indx+1} has been updated.", color=0x6abe30)
             await message.reply(embed=embed)
             await message.add_reaction('✅')
+    
         except Exception as err:
             await utils.ExceptionReplyEmbed(message=message, error=f"Run-Time Exception: {err}", desc="Please check your task ID and try again.")
             return
@@ -276,6 +307,7 @@ async def DeleteTask(task_id: str , message :discord.Message):
     try:
         useracc = utils.GetAcc(message.author.id)
         useracc.DelTask(task_id=int(task_id))
+
         embed = discord.Embed(title="Task Deleted", description=f"Task {task_id+1} has been deleted.", color=0x6abe30)
         await message.reply(embed=embed)
         await message.add_reaction('✅')
@@ -289,6 +321,7 @@ async def DeleteAllTasks(message : discord.Message):
         # Delete all tasks in reverse order
         for i in range(len(useracc.tasks)-1,-1,-1):
             useracc.DelTask(task_id=i)
+        
         embed = discord.Embed(title="All Tasks Deleted", description="All your tasks have been deleted.", color=0x6abe30)
         await message.reply(embed=embed)
     else:
